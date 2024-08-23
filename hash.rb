c1e01b499c3a425bf38e0f64bbcf3816a91facda
Fauzan <fauzandwimulyono@gmail.com>
2024-08-23 21:04:07 +0700

    Add ruby code

---
+++ hash.rb
@@ -0,0 +1,1 @@
+  {:dog => 'barks', :cat => 'meow', :pig => 'oinks'}[:cat] #=> "meow"
--- numb.rb
+++ numb.rb
@@ -12,4 +12,14 @@
 8 % 2 #=> 0
 10 % 4 #=> 2 
 
+4 == 4 #=> true
+5 == 4 #=> false
+
+"foo" == "bar" #=> false
+"foo" == "foo" #=> true
+
+'4' == 4 #=> false (different types)
+
+'12'.to_i #=> 12
+ 12.to_s #=> '12'
 
