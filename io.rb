30679604de2c362c98b2b99ebd97fc457a675f0b
Fauzan <fauzandwimulyono@gmail.com>
2024-08-25 14:42:51 +0700

    Add if else file

---
+++ io.rb
@@ -0,0 +1,8 @@
+print 'Hello' # It will return in one line
+puts 'Hello' # It will return in two line
+gets #=> return the user input
+
+new_string = gets.chomp # Return user input in variable
+puts new_string = #=> Return the variable
+
+
