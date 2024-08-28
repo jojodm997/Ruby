a3f2eededc0c3b79ca3cabe9c6fc2f0825ccf39e
Fauzan <fauzandwimulyono@gmail.com>
2024-08-28 21:06:28 +0700

    Add p description

--- io.rb
+++ io.rb
@@ -5,4 +5,13 @@ gets #=> return the user input
 new_string = gets.chomp # Return user input in variable
 puts new_string = #=> Return the variable
 
+# This code will prompt the user to enter a value, which will be stored in a variable va; and finally will be printed on STDOUT  
+puts "Enter a value :"
+val = gets
+puts val
+
+# p is useful for debugging because it return raw value
+p "Hello world"
+#=> "Hello world" 
+
 
