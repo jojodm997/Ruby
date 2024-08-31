7bbe261aa82ba4d57938431594909ba01358a8c9
Fauzan <fauzandwimulyono@gmail.com>
2024-08-31 22:14:28 +0700

    Add array methods

--- arr.rb
+++ arr.rb
@@ -55,6 +55,58 @@ a.concat(b) #=> [1, 2, 3, 3, 4, 5]
 
 [1, 2, 3].include?(3) #=> true 
 [1, 2, 3].include?("3") #=> false 
+[1, 2, 3].include?(4) #=> false 
 
 [1, 2, 3].join #=> "123"
 [1, 2, 3].join("-") #=> "1-2-3"
+
+[1, 2, 3, 4, [5, 6], [7, 8]].flatten #=> [1, 2, 3, 4, 5, 6, 7, 8]
+
+arrys = [1, 2, 3]
+arrys.first #=> 1 
+arrys.last #=> 3
+
+a = [1, 2, 3, 4]
+a.map { |num| num**2 } #=> [1. 4. 9. 16]
+a.collect { |num| num**2 } #=> [1, 4, 9, 16]
+
+a.delete_at(1) # 2
+a #=> [1, 3, 4]
+
+my_pets = ["cat", "dog", "bird", "cat", "snake"]
+my_pets.delete("cat") #=> "cat"
+my_pets #=> ["dog, bird, snake"]
+
+b = [1, 1, 2, 2, 3, 3, 4, 4] 
+b.uniq #=> [1, 2, 3, 4]
+b #=> [1, 1, 2, 2, 3, 3, 4, 4]
+b.uniq! #=> [1, 2, 3, 4]
+b #=> [1, 2, 3, 4]
+
+# uniq and uniq! are two different methods for Ruby Arrays. You cannot simply append a ! onto any method and achieve a destructive operation.
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+numbers.select { |numb| numb > 4 } #=> [5, 6, 7, 8, 9, 10]
+numbers #=> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+teams = [["Joe", "Steve"], ["Frank", "Molly"], ["Dan", "Sara"]]
+teams[1] #=> ["Frank", "Molly"]
+
+yoink = [2, 3, 4]
+yoink.unshift(1) #=> [1, 2, 3, 4]
+"It's easy as #{yoink}" #=> "It's easy as [1, 2, 3, 4]" ruby does to_s behind the scene
+
+[1, 2, 3].each_index { |i| puts "This is index #{i}" }
+#=> This is index 0 
+#=> This is index 1 
+#=> THis is index 2 
+
+[1, 2, 3].each_with_index { |val, idx| puts "#{idx+1}. #{val}" }
+#=> 1. 1
+#=> 2. 2
+#=> 3. 3
+
+[5, 3, 8, 2, 4, 1].sort #=> [1, 2, 3, 4, 5, 8]
+[1, 2, 3].product([4, 5]) #=> [[1, 4], [1, 5], [2, 4], [2, 5], [3, 4], [3, 5]]
+
+
