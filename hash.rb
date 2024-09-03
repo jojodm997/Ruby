eff0e7ada0f05025a5aaad3e7eb07864fdb97468
Fauzan <fauzandwimulyono@gmail.com>
2024-09-03 21:43:58 +0700

    Update hash

--- hash.rb
+++ hash.rb
@@ -37,3 +37,19 @@ hash1 = { "a" => 100, "b" => 200}
 hash2 = { "b" => 254, "c" => 300}
 hash1.merge(hash2) #=> { "a" => 100, "b" => 254, "c" => 300}
 
+# Rocket syntax
+american_cars = { 
+  :chevrolet => "Corvette",
+  :ford => "Mustang",
+  :dodge => "Ram"
+}
+
+# Symbols syntax 
+japanese_cars { 
+  honda: "Accord",
+  toyota: "Corrola",
+  nissan: "Altima"
+}
+
+american_cars[:ford] #=> Mustang 
+japanese_cars[:honda] #=> Accord 
