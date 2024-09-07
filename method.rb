f67d6cc9843e01348a92078aa2f2914261c7d10f
Fauzan <fauzandwimulyono@gmail.com>
2024-09-07 20:51:53 +0700

    Add new script

--- method.rb
+++ method.rb
@@ -25,4 +25,34 @@ puts even_odd("Ruby") #=> A number was not entered.
 puts even_odd(16) #=> That's an even number.
 puts even_odd(17) #=> That's an odd number.
 
+phrase = ["be", "to", "not", "or", "be", "to"]
+puts phrase.reverse.join(" ").capitalize
+#=> "To be or not to be"
+
+puts 5.even? #=> false
+puts 6.even? #=> true
+puts 17.odd? #=> true 
+
+puts 12.between?(10, 15) #=> true 
+
+whisper = "HELLO EVERYBODY"
+puts whisper.downcase #=> "HELLO EVERYBODY"
+puts whisper #=> Without bang ! "HELLO EVERYBODY" with bang whisper.downcase! "hello everybody"
+
+def add(a, b)
+  a + b 
+end 
+add(20, 45) #=> 65
+
+def subtract(a, b) 
+  a - b 
+end 
+subtract(80, 10) #=> 70
+
+def multiply(num1,num2)
+  num1 * num2 
+end 
+multiply(add(20, 45), subtract(80, 10)) #=> 65 + 70 = 4550
+add(subtract(80, 10), multiply(subtract(20, 6), add(30, 5))) #=> 70 + 14 * 35 = 490
+
 
