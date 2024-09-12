4c6b5de4db2b876ecc160923f7240a34ba310926
Fauzan <fauzandwimulyono@gmail.com>
2024-09-12 15:10:01 +0700

    Add emethod

--- emethod.rb
+++ emethod.rb
@@ -19,10 +19,94 @@ my_hash.each { |pair| puts "the pair is #{pair}" }
 fruits = ["apple", "banana", "strawberry", "pineapple"]
 
 fruits.each_with_index { |fruit, index| puts fruit if index.even? }
-
 #=> apple 
 #=> strawberry 
 #=> ["apple", "banana", "strawberry", "pineapple"]
 
 friends.map { |friend| friend.upcase }
 #=> ["SHARON", "LEO", "LEILA", "BRIAN", "ARUN"]
+
+my_order = ["medium Big Mac", "medium fries", "medium milkshake"]
+
+my_order.map { |item| item.gsub("medium", "extra large") }
+#=> ["extra large Big Mac", "extra large fries", "extra large milkshake"]
+
+salaries = [1200, 1500, 1100, 1000]
+
+salaries.map { |salary| salary - 700 }
+#=> [500, 800, 400, 1100]
+
+responses = { "Sharon" => "yes", "Leo" => "no", "Leila" => "no", "Arun" => "yes" }
+responses.select { |person, responses| responses == "yes" }
+#=> {"Sharon"=>"yes", "Arun"=>"yes"}
+
+my_numbers = [5, 6, 7, 8]
+
+my_numbers.reduce { |sum, number| sum + number }
+#=> 26
+
+my_number = [5, 6, 7, 8]
+
+my_number.reduce(1000) { |sum, number| sum + number }
+#=> 1026
+
+votes = ["Bob's Dirty Burger Shack", "St. Mark's Bistro", "Bob's Dirty Burger Shack"]
+
+votes.reduce(Hash.new(0)) do |result, vote|
+  result[vote] += 1 
+  result
+end 
+#=> {"Bob's Dirty Burger Shack"=>2, "St. Mark's Bistro"=>1}
+
+friends = ["Sharon", "Leo", "Leila", "Brian", "Arun"]
+
+friends.map! { |friend| friend.upcase }
+#=> ["SHARON", "LEO", "LEILA", "BRIAN", "ARUN"]
+friends 
+#=> ["SHARON", "LEO", "LEILA", "BRIAN", "ARUN"] mutate the original value 
+
+friendd = ["Sharon", "Leo", "Leila", "Brian", "Arun"]
+
+invited_friends = friendd.select { |friend| friend != "Brian" }
+
+friendd 
+#=> ["Sharon", "Leo", "Leila", "Brian", "Arun"]
+
+invited_friends 
+#=> ["Sharon", "Leo", "Leila", "Arun"]
+
+def invited_friends(friends)
+  friendd.select { |friend| friend != "Brian" }
+end 
+
+friends 
+#=> ["Sharon", "Leo", "Leila", "Brian", "Arun"]
+
+invited_friends(friends)
+#=> ["Sharon", "Leo", "Leila", "Arun"]
+
+my_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 100]
+
+my_array.select { |item| item % 2 == 0 } #=> [2, 4, 6, 8, 100]
+
+my_array.collect { |num| num ** 2 } #=> [1, 4, 9, 16, 25, 36, 49, 64, 81, 10000]
+
+my_hash = {"Joe" => "male", "Jim" => "male", "Patty" => "female"}
+my_hash.select { |name, gender| gender == "male" }
+#=> {"Joe" => "male", "Jim" => "male"}
+
+names = ["James", "Bob", "Joe", "Mark", "Jim"]
+names.group_by { |name| name.length } 
+#=> {5=>["James"], 3=>["Bob", "Joe", "Jim"], 4=>["Mark"]}
+names.grep(/J/) #=> ["James", "Joe", "Jim"]
+
+["11", "21", "5"].map(&:to_i) #=> [11, 21, 5]
+
+
+
+
+
+
+
+
+
