f64c23d48bbb2ecad939ce797c5fef271069862a
Fauzan <fauzandwimulyono@gmail.com>
2024-09-25 19:56:51 +0700

    init

---
+++ substrings.rb
@@ -0,0 +1,6 @@
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings(str, dict)
+end
+
+substrings("below", dictionary) # expected   => { "below" => 1, "low" => 1 }
