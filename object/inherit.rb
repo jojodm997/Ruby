d2a56f8bb10d0fe44ef7067d8b303c0fb7ded0f9
Fauzan <fauzandwimulyono@gmail.com>
2024-10-08 20:59:02 +0700

    Remove exercises directory

--- object/inherit.rb
+++ object/inherit.rb
@@ -93,6 +93,48 @@ module Conversions
 end
 
 
+class Dog 
+  DOG_YEARS = 7 
+
+  attr_accessor :name, :age 
+
+  def initialize(n, a)
+    self.name = n 
+    self.age = a
+  end
+
+  private 
+
+  def human_years 
+    age * DOG_YEARS 
+  end
+end
+
+sparky = Dog.new("Sparky", 4)
+sparky.human_years
+# NoMethodError: private method `human_years' called for
+  #<GoodDog:0x007f8f431441f8 @name="Sparky", @age=4> 
 
 
+class Person 
+  def initialize(age)
+    @age = age 
+  end
 
+  def older?(other_person)
+    age > other_person.age
+  end
+  
+  protected
+
+  attr_reader :age 
+end
+
+malory = Person.new(64)
+sterling = Person.new(42)
+
+malory.older?(sterling) #=> true 
+sterling.older?(malory) #+> false 
+
+malory.age 
+# => NoMethodError: protected method `age' called for #<Person: @age=64>
