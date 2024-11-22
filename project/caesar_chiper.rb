fecca7561c902eedab455a82c4ead5dacbca3f9a
Fauzan <fauzandwimulyono@gmail.com>
2024-11-22 11:54:54 +0700

    Change operator

--- project/caesar_chiper.rb
+++ project/caesar_chiper.rb
@@ -6,12 +6,12 @@ def chiper(str, num)
   str.chars do |char| 
     if upcase.include?(char)
       new_index = (upcase.index(char) + num ) % 26
-      encrypted += upcase[new_index]
+      encrypted << upcase[new_index]
     elsif lowcase.include?(char)
       new_index = (lowcase.index(char) + num ) % 26
-      encrypted += lowcase[new_index]
+      encrypted << lowcase[new_index]
     else 
-      encrypted += char
+      encrypted << char
     end
   end
     return encrypted
